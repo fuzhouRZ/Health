cceff67a3a38ed40ee46f036fd7a249f00cb49e3
zhangfu <752323877@qq.com>
2016-07-06 17:41:30 +0800

    .

--- app/src/main/java/com/ie1e/health/MainActivity.java
+++ app/src/main/java/com/ie1e/health/MainActivity.java
@@ -1,67 +1,79 @@
 package com.ie1e.health;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
-import com.alibaba.sdk.android.man.MANService;
-import com.alibaba.sdk.android.man.MANServiceProvider;
-import com.ie1e.health.Util.ImageUtil;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageView iv_zhzx,iv_yyjy,iv_jksj,iv_tsfw,iv_ylzx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        ImageView testImage = (ImageView) findViewById(R.id.testImage);
-        ImageUtil.loader("https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=1784234501,600865947&fm=58", testImage);
-
-        MANService manService = MANServiceProvider.getService();
-        // 注册用户埋点
-        manService.getMANAnalytics().userRegister("usernick");
-
-        // 用户登录埋点
-        manService.getMANAnalytics().updateUserAccount("usernick", "102123354742kjhhjf");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
+        initView();
 
-        return super.onOptionsItemSelected(item);
+
+
+
+
+
+//        MANService manService = MANServiceProvider.getService();
+//        // 注册用户埋点
+//        manService.getMANAnalytics().userRegister("usernick");
+//
+//        // 用户登录埋点
+//        manService.getMANAnalytics().updateUserAccount("usernick", "102123354742kjhhjf");
+    }
+
+    private void initView() {
+        iv_zhzx = (ImageView) findViewById(R.id.iv_zhzx);
+        iv_yyjy = (ImageView) findViewById(R.id.iv_yyjy);
+        iv_jksj = (ImageView) findViewById(R.id.iv_jksj);
+        iv_tsfw = (ImageView) findViewById(R.id.iv_tsfw);
+        iv_ylzx = (ImageView) findViewById(R.id.iv_ylzx);
+
+        iv_zhzx.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            //账户中心
+            case R.id.iv_zhzx:
+
+                break;
+            //健康数据
+            case R.id.iv_jksj:
+
+                break;
+            //特色服务
+            case R.id.iv_tsfw:
+
+                break;
+            //预约就医
+            case R.id.iv_yyjy:
+
+                break;
+            //医疗咨询
+            case R.id.iv_ylzx:
+
+                break;
+            default:
+                break;
+        }
     }
 }
